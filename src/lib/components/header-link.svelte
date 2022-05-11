c91dc570fa4dd4d7ecbdf7cf38e516bd912d91a4
Milan Jaritz <milan.jaritz@gmail.com>
2022-05-11 19:38:32 +0200

    try new mobile menu

--- src/lib/components/header-link.svelte
+++ src/lib/components/header-link.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   export let href: string;
+  export let n: string;
 </script>
 
-<a {href} class="hover:bg-blue-500/10 transition h-full px-2">
+<a {href} class="hover:bg-blue-500/10 transition h-full px-2 font-mono" on:click>
   <div class="h-full flex items-center">
+    <span class="font-mono text-blue-500">{n}</span>
     <slot />
   </div>
 </a>
--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,24 +1,50 @@
-<script>
+<script lang="ts">
+  import Icon from "@iconify/svelte";
+  import { fly } from "svelte/transition";
   import HeaderLink from "./header-link.svelte";
+
+  let isOpen = false;
+  let windowWidth = 1000;
+
+  $: isMobile = windowWidth < 640;
 </script>
 
-<div
-  class="fixed w-full left-0 top-0 sm:h-20 h-16 flex items-center text-gray-700 sm:px-12 px-2 bg-white/90 z-50"
-  style="box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1); border: 1px solid rgba(255, 255, 255, 0.3);"
->
-  <div class="h-full items-center sm:gap-1 flex">
-    <HeaderLink href="#about">About</HeaderLink>
-    <HeaderLink href="#projects">Projects</HeaderLink>
-    <HeaderLink href="#contact">Contact</HeaderLink>
-  </div>
-
-  <div class="flex-grow" />
-
-  <a
-    href="/cv_milan.pdf"
-    target="_blank"
-    class="border border-blue-500 rounded px-4 py-2 font-bold text-blue-500 hover:bg-blue-500 hover:text-white transition"
+{#if isOpen || !isMobile}
+  <div
+    transition:fly={{ y: 200 }}
+    class="fixed w-full left-0 sm:top-0 bottom-0 sm:h-20 flex sm:items-center items-end text-gray-700 sm:px-12 px-8 py-5 sm:py-0 bg-white/90 z-50 flex-row-reverse sm:flex-row"
+    style="box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1); border: 1px solid rgba(255, 255, 255, 0.3);"
   >
-    Resume
-  </a>
-</div>
+    <div class="h-full w-full items-end sm:items-center gap-3 sm:gap-5 flex flex-col sm:flex-row">
+      {#if isMobile}
+        <div on:click={() => (isOpen = false)}>
+          <Icon icon="mdi:close" class="w-8 h-8 text-gray-400" />
+        </div>
+      {/if}
+      <HeaderLink n="01." href="#about" on:click={() => (isOpen = false)}>About</HeaderLink>
+      <HeaderLink n="02." href="#projects" on:click={() => (isOpen = false)}>Projects</HeaderLink>
+      <HeaderLink n="03." href="#contact" on:click={() => (isOpen = false)}>Contact</HeaderLink>
+    </div>
+
+    <div class="flex-grow" />
+
+    <a
+      href="/cv_milan.pdf"
+      target="_blank"
+      class="border border-blue-500 rounded px-4 py-2 font-bold text-blue-500 hover:bg-blue-500 hover:text-white transition"
+    >
+      Resume
+    </a>
+  </div>
+{/if}
+
+{#if !isOpen && isMobile}
+  <div
+    on:click={() => (isOpen = !isOpen)}
+    class="bg-gray-100/80 border rounded-full fixed bottom-2 right-2 p-4"
+  >
+    <Icon icon="mdi:menu" class="w-10 h-10 text-gray-500" />
+  </div>
+{/if}
+
+<svelte:window bind:innerWidth={windowWidth} />
