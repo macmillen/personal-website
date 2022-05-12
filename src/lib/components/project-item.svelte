ad0b450badc89c4302a09acf7842ea9ae7168b98
Milan Jaritz <milan.jaritz@gmail.com>
2022-05-12 17:43:23 +0200

    rename work => project

--- src/lib/components/project-item.svelte
+++ src/lib/components/project-item.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import type { Tech } from "$lib/types/types";
   import Icon from "@iconify/svelte";
+  import ProjectItemImage from "./project-item-image.svelte";
+  import ProjectItemLink from "./project-item-link.svelte";
   import TechItem from "./tech-item.svelte";
-  import WorkItemImage from "./work-item-image.svelte";
-  import WorkItemLink from "./work-item-link.svelte";
 
   export let text: string;
   export let techs: Tech[];
@@ -16,15 +16,15 @@
 <div class="max-w-screen-sm mb-10">
   <div class="flex items-center gap-3 mt-10">
     <Icon icon="mdi:folder" class="w-10 h-10 text-gray-500 hidden sm:block" />
-    <WorkItemImage class="sm:hidden block w-20 h-20" {imgSrc} />
+    <ProjectItemImage class="sm:hidden block w-20 h-20" {imgSrc} />
     <div class="flex justify-between w-full items-center flex-wrap gap-2">
       <h2 class="text-xl text-gray-700">{title}</h2>
       <div class="flex gap-2">
         {#if link}
-          <WorkItemLink icon="mdi:open-in-new" {link} />
+          <ProjectItemLink icon="mdi:open-in-new" {link} />
         {/if}
         {#if github}
-          <WorkItemLink icon="mdi:github" link={github} />
+          <ProjectItemLink icon="mdi:github" link={github} />
         {/if}
       </div>
     </div>
@@ -32,7 +32,7 @@
   <div class="flex mb-4 gap-3" />
 
   <div class="flex gap-10">
-    <WorkItemImage class="hidden sm:block" {imgSrc} />
+    <ProjectItemImage class="hidden sm:block" {imgSrc} />
     <div class="flex flex-col">
       <p class="text-gray-600">{text}</p>
       <div class="flex-grow" />
--- src/lib/components/project.svelte
+++ src/lib/components/project.svelte
@@ -1,19 +1,12 @@
 <script>
+  import ProjectItem from "./project-item.svelte";
   import Section from "./section.svelte";
-  import WorkItem from "./work-item.svelte";
 </script>
 
 <Section id="projects" n="02.">Projects</Section>
 
 <div class="flex flex-col divide-y -mt-10">
-  <WorkItem
-    link="https://lettranslate.com/"
-    text="Intoducing a new way to localize your products. Distribute each individual team member across multiple languages and projects so that your resources stay safe and protected. Get a high confidence by being able to traverse the history of each translation. That way mistakes can be easily undone while keeping a detailed and clean activity log."
-    techs={["svelte", "typescript", "graphql", "prisma", "postgres", "tailwind"]}
-    imgSrc="/projects/lettranslate.jpg"
-    title="lettranslate.com"
-  />
-  <WorkItem
+  <ProjectItem
     link="https://github-changelogger.vercel.app/"
     github="https://github.com/macmillen/github-changelogger"
     text="GitHub ChangeLogger is a tool to track changes across public code repositories hosted on github.com. It only uses the public GitHub API and requires no authentication. The app works without any outbound network traffic and all data is stored inside the local storage."
@@ -21,35 +14,35 @@
     imgSrc="/projects/changelogger.jpg"
     title="ChangeLogger"
   />
-  <WorkItem
+  <ProjectItem
     github="https://github.com/macmillen/tool23"
     text="An app that works with geo data and spherical distances to show available items in your area for rental. (student project)"
     techs={["ionic", "angular", "typescript", "nodejs", "mongodb"]}
     imgSrc="/projects/tool23.jpg"
     title="Tool23"
   />
-  <WorkItem
+  <ProjectItem
     link="https://mymdb-d5846.firebaseapp.com/"
     text="A webapp where you can save all of your favorite movies, series and games. Create watchlists and share them with your friends!"
     techs={["angular", "typescript", "nodejs", "mongodb", "firebase"]}
     imgSrc="/projects/mymdb.jpg"
     title="MyMDb"
   />
-  <WorkItem
+  <ProjectItem
     link="https://roommate-matcher-df7ae.firebaseapp.com/"
     text="The purpose of the project is to ensure new students are paired with a compatible roommate at the VIA University Collage. (student project)"
     techs={["angular", "typescript", "firebase"]}
     imgSrc="/projects/roommate_matcher.jpg"
     title="Roommate Matcher"
   />
-  <WorkItem
+  <ProjectItem
     link="https://space-chaos.web.app/"
     text="A little Space Shooter game."
     techs={["js", "p5"]}
     imgSrc="/projects/space_chaos.jpg"
     title="Space Chaos"
   />
-  <WorkItem
+  <ProjectItem
     link="https://macmillen.github.io/p5_tetris/src/"
     github="https://github.com/macmillen/p5_tetris"
     text="A Tetris clone."
@@ -57,7 +50,7 @@
     imgSrc="/projects/tetris.jpg"
     title="Tetris"
   />
-  <WorkItem
+  <ProjectItem
     link="https://macmillen.github.io/p5_gaussian_distribution/"
     github="https://github.com/macmillen/p5_gaussian_distribution"
     text="Simulation of two n-sided dices."
@@ -65,14 +58,14 @@
     imgSrc="/projects/gaussian_distribution.jpg"
     title="Gaussian Distribution"
   />
-  <WorkItem
+  <ProjectItem
     github="https://github.com/macmillen/processing_maze_solver"
     text="Brute force algorithm to find a path to the and of the labyrinth."
     techs={["processing"]}
     imgSrc="/projects/maze_solver.jpg"
     title="Maze Solver"
   />
-  <WorkItem
+  <ProjectItem
     github="https://github.com/macmillen/3D_visualization"
     text="A really cool 3D visualization tool to play around with. You can choose from different shapes like cube, mandala and dodecahedron while being able to activate all kinds of graphic effects."
     techs={["processing"]}
